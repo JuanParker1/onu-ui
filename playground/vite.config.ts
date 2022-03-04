f078cc6fc24738a39fb369c3b16692a152d77244
chris-zhu <1633711653@qq.com>
2022-03-04 18:37:28 +0800

    feat: unocss config

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    Vue(),
+    Components({
+      dirs: ['../src/components'],
+      dts: ''
+    })
+  ]
 })
